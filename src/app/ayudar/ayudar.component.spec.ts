bc55d27cc8c16aa31abb0dcf07f37c875257ce15
DaNiELChIoRo <da.leon1917@gmail.com>
2018-09-19 00:35:05 -0500

    second commit

---
+++ src/app/ayudar/ayudar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AyudarComponent } from './ayudar.component';
+
+describe('AyudarComponent', () => {
+  let component: AyudarComponent;
+  let fixture: ComponentFixture<AyudarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AyudarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AyudarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/quienes-somos/quienes-somos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quienes-somos',
+  templateUrl: './quienes-somos.component.html',
+  styleUrls: ['./quienes-somos.component.css',
+              '../../assets/theme.shortcodes.css',
+              '../../assets/responsive.css',
+              '../../assets/default.css']
+})
+export class QuienesSomosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
