d9b0c965be41d14196f2dbb9d1650dd3facfbe68
DaNiELChIoRo <da.leon1917@gmail.com>
2018-09-20 09:11:51 -0500

    20-09-18 first commit of the day

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css',
+              '../../assets/theme.shortcodes.css',
+              '../../assets/responsive.css',
+              '../../assets/default.css']
 })
 export class InicioComponent implements OnInit {
 
