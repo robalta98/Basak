6d3921214578f8a767851097861749479d6ec046
DaNiELChIoRo <da.leon1917@gmail.com>
2018-09-26 17:12:34 -0500

    26-09-2018

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,9 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css',
+              './inicio.style.css',
               '../../assets/theme.shortcodes.css',
               '../../assets/responsive.css',
-              '../../assets/default.css']
+              '../../assets/default.css',
+              '../../assets/css/settings.css',
+              '../../assets/fonts/css/fontello.css']
 })
 export class InicioComponent implements OnInit {
 
