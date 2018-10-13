8f2b64c53e0e8ab711fbdf765bc613b0149ae2b5
DaNiELChIoRo <da.leon1917@gmail.com>
2018-10-13 11:49:52 -0500

    first commit fo the day 13-10-18 nuestros programas finalizada sin imagenes y revision de diseño

--- src/app/nuestros-programas/nuestros-programas.component.ts
+++ src/app/nuestros-programas/nuestros-programas.component.ts
@@ -3,11 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nuestros-programas',
   templateUrl: './nuestros-programas.component.html',
-  styleUrls: ['./nuestros-programas.component.css']
+  styleUrls: ['./nuestros-programas.component.css',
+              '../../assets/theme.shortcodes.css',
+              '../../assets/responsive.css',
+              '../../assets/default.css']
 })
 export class NuestrosProgramasComponent implements OnInit {
 
-  constructor() { }
+  items = [];
+
+  constructor() {
+    this.items = ['Conferencias, pláticas y talleres a empresas.', 'Conferencias para adolescents y niños.',
+                  'Conferencias para padres de familia', 'Tratamientos psicológicos y psiquiátricos  en nuestro centro de desarrollo humano para toda la familia',
+                  'Detección temprana hacia un comportamiento con características adictivas o perfil adictivo'];
+  }
 
   ngOnInit() {
   }
